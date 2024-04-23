58da371bdca380086770bf92e62b251070f5bb78
Alex Fritzel <alex@Alexs-MacBook-Air.local>
2024-04-22 19:12:57 -0500

    .

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+console.log("test");
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
